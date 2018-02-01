8f5f28b1a77328368a73bc6b0a471283a38098e1
sty945 <sutaoyu@yeah.net>
2018-02-01 20:17:25 +0800

    添加邮箱验证码的数据库文件

--- users/models.py
+++ users/models.py
@@ -1,9 +1,14 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
 class UserProfile(AbstractUser):
+    '''
+    用户个人信息
+    '''
     nick_name = models.CharField(max_length=50, verbose_name=u'呢称', default="")
     birthday = models.DateField(verbose_name=u'生日', null=True, blank=True)
     gender = models.CharField(max_length=5, choices=(("male", u'男'), ("female", u'女')), default="female")
@@ -17,3 +22,17 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class EmailVerifyRecord(models.Model):
+    '''
+    邮箱验证码
+    '''
+    code = models.CharField(max_length=20, verbose_name=u'验证码')
+    email = models.EmailField(max_length=50, verbose_name=u'邮箱')
+    send_type = models.CharField(choices=(('register', u'注册'), ('forget' u'找回密码')), max_length=10)
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = u'邮箱验证码'
+        verbose_name_plural = verbose_name
+
