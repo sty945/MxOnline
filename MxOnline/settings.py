c403c13bbb468cd3ea1eea1d7199a83d6ec3bef2
sty945 <sutaoyu@yeah.net>
2018-02-02 14:47:09 +0800

    将所有APP的名称添加到setting中去

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'users'
+    'users',
+    'courses',
+    'operation',
+    'organization',
 ]
 AUTH_USER_MODEL = "users.UserProfile"
 
