5fe1b2b5fc4e9d1ff2f181d6cf644d0d9ca87ebb
sty945 <sutaoyu@yeah.net>
2018-02-02 11:39:53 +0800

    添加用户咨询数据库信息

--- operation/models.py
+++ operation/models.py
@@ -1,3 +1,19 @@
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+class UserAsk(models.Model):
+    """
+    用户咨询数据库表信息
+    """
+    name = models.CharField(max_length=20, verbose_name=u'姓名')
+    mobile = models.CharField(max_length=11, verbose_name=u'手机')
+    course_name = models.CharField(max_length=50, verbose_name=u'课程名')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'用户咨询'
+        verbose_name_plural = verbose_name
+
