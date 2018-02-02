dc8d8958cfdfcc68cdd98f706a1fba973a4f606c
sty945 <sutaoyu@yeah.net>
2018-02-02 14:07:09 +0800

    添加课程数据库信息

--- operation/models.py
+++ operation/models.py
@@ -2,6 +2,9 @@ from datetime import datetime
 
 from django.db import models
 
+from users.models import UserProfile
+from courses.models import Course
+
 
 # Create your models here.
 class UserAsk(models.Model):
@@ -17,3 +20,16 @@ class UserAsk(models.Model):
         verbose_name = u'用户咨询'
         verbose_name_plural = verbose_name
 
+
+class CourseComments(models.Model):
+    """
+    课程评论数据库信息
+    """
+    user = models.ForeignKey(UserProfile, verbose_name=u'用户')
+    course = models.ForeignKey(Course, verbose_name=u'课程')
+    comments = models.CharField(max_length=200, verbose_name=u'评论')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'课程评论'
+        verbose_name_plural = verbose_name
