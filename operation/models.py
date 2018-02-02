c46403515a488c2b6786fee174341bb5f367af72
sty945 <sutaoyu@yeah.net>
2018-02-02 14:25:13 +0800

    添加用户消息数据库信息

--- operation/models.py
+++ operation/models.py
@@ -48,3 +48,21 @@ class UserFavorite(models.Model):
         verbose_name = u'用户收藏'
         verbose_name_plural = verbose_name
 
+
+class UserMessage(models.Model):
+    """
+    用户消息数据库信息
+    """
+    # 因为我们的消息有两种:发给全员和发给某一个用户。
+    # 所以如果使用外键，每个消息会对应要有用户。很难实现全员消息。
+    # 机智版 为0发给所有用户，不为0就是发给用户的id
+    user = models.IntegerField(default=0, verbose_name=u'接受用户')
+    message = models.CharField(max_length=500, verbose_name=u'消息内容')
+    # 是否已读: 布尔类型 BooleanField False未读,True表示已读
+    has_read = models.BooleanField(default=False, verbose_name=u'是否已读')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'用户消息'
+        verbose_name_plural = verbose_name
+
