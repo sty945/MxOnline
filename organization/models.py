b23d9ab0b1dddaada21ca45f1d3a02ec3ebfa501
sty945 <sutaoyu@yeah.net>
2018-02-02 10:52:46 +0800

    添加课程机构基本信息，教师基本信息，城市信息的数据库信息

--- organization/models.py
+++ organization/models.py
@@ -1,3 +1,54 @@
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+class CityDict(models.Model):
+    """
+    城市数据库表
+    """
+    name = models.CharField(max_length=20, verbose_name=u'城市')
+    desc = models.CharField(max_length=20, verbose_name=u'描述')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'城市'
+        verbose_name_plural = verbose_name
+
+
+class CourseOrg(models.Model):
+    """
+    课程机构基本信息表
+    """
+    city = models.ForeignKey(CityDict, verbose_name=u'所在城市')
+    name = models.CharField(max_length=50, verbose_name=u'机构名称')
+    desc = models.TextField(verbose_name=u'机构描述')
+    click_num = models.IntegerField(default=0, verbose_name=u'点击数')
+    fav_nums = models.IntegerField(default=0, verbose_name=u'收藏数')
+    image = models.ImageField(upload_to="org/%Y/%m", verbose_name=u'封面图')
+    address = models.CharField(max_length=150, verbose_name=u'机构地址')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'课程机构'
+        verbose_name_plural = verbose_name
+
+
+class Teacher(models.Model):
+    """
+    教师信息基本信息表
+    """
+    org = models.ForeignKey(CourseOrg, verbose_name=u'所属机构')
+    name = models.CharField(max_length=50, verbose_name=u'教师名称')
+    work_years = models.IntegerField(default=0, verbose_name=u'工作年限')
+    work_company = models.CharField(max_length=50, verbose_name=u'就职公司')
+    work_position = models.CharField(max_length=50, verbose_name=u'公司职位')
+    points = models.CharField(max_length=50, verbose_name=u'教学特点')
+    click_num = models.IntegerField(default=0, verbose_name=u'点击数')
+    fav_nums = models.IntegerField(default=0, verbose_name=u'收藏数')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'教师'
+        verbose_name_plural = verbose_name
