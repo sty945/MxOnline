7f499fa0d0cfdc1afab03a43e142f482d22de350
sty945 <sutaoyu@yeah.net>
2018-02-02 23:14:04 +0800

    再xadmin中创建轮播图的信息

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -9,13 +9,26 @@
 # File: adminx.py
 import xadmin
 
-from .models import EmailVerifyRecord
+from .models import EmailVerifyRecord, Banner
 
 
 class EmailVerifyRecordAdmin(object):
+    """
+    将邮箱验证信息添加到xadmin中来
+    """
     list_display = ['code', 'email', 'send_type', 'send_time']
     search_fields = ['code', 'email', 'send_type']
     list_filter = ['code', 'email', 'send_type', 'send_time']
 
 
+class BannerAdmin(object):
+    """
+    添加轮播图信息到xadmin中来
+    """
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
