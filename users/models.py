7463b346101502505275d2e8e4af01e40bb5cd1d
sty945 <sutaoyu@yeah.net>
2018-02-02 14:51:29 +0800

    修改choices中的一个小错误

--- users/models.py
+++ users/models.py
@@ -29,7 +29,7 @@ class EmailVerifyRecord(models.Model):
     '''
     code = models.CharField(max_length=20, verbose_name=u'验证码')
     email = models.EmailField(max_length=50, verbose_name=u'邮箱')
-    send_type = models.CharField(choices=(('register', u'注册'), ('forget' u'找回密码')), max_length=10)
+    send_type = models.CharField(choices=(('register', u'注册'),('forget', u'找回密码')), max_length=10)
     send_time = models.DateTimeField(default=datetime.now)
 
     class Meta:
