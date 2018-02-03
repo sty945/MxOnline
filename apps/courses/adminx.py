eafe11733545fd9af5a941ff63518d46d9956201
sty945 <sutaoyu@yeah.net>
2018-02-03 10:27:02 +0800

    将course的一些信息添加进去

--- apps/courses/adminx.py
+++ apps/courses/adminx.py
@@ -7,3 +7,19 @@
 # Author: sty
 
 # File: adminx.py
+
+from .models import Course, Lesson, Video, CourseResources
+
+import xadmin
+
+
+class CourseAdmin(object):
+    """
+    课程的管理
+    """
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums']
+    search_fields = ['name', 'desc', 'detail', 'degree', 'students', 'fav_nums']
+    list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums']
+
+
+xadmin.site.register(Course, CourseAdmin)
