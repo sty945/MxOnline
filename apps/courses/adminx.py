9c1a2a3859405ecd9631566891c9dcaabb198107
sty945 <sutaoyu@yeah.net>
2018-02-03 11:36:48 +0800

    添加视频管理，章节管理，课程资源的管理

--- apps/courses/adminx.py
+++ apps/courses/adminx.py
@@ -22,4 +22,34 @@ class CourseAdmin(object):
     list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'fav_nums']
 
 
+class LessonAdmin(object):
+    """
+    章节的管理
+    """
+    list_display = ['course', 'name', 'add_time']
+    search_fields = ['course', 'name']
+    list_filter = ['course__name', 'name', 'add_time']
+
+
+class VideoAdmin(object):
+    """
+    视频的管理
+    """
+    list_display = ['lesson', 'name', 'add_time']
+    search_fields = ['lesson', 'name']
+    list_filter = ['lesson', 'name', 'add_time']
+
+
+class CourseResourcesAdmin(object):
+    """
+    课程资源的管理
+    """
+    list_display = ['course', 'name', 'download', 'add_time']
+    search_fields = ['course', 'name', 'download']
+    list_filter = ['course', 'name', 'download', 'add_time']
+
+
 xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(Lesson, LessonAdmin)
+xadmin.site.register(Video, VideoAdmin)
+xadmin.site.register(CourseResources, CourseResourcesAdmin)
