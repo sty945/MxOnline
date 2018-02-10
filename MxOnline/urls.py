e39ad67bd0f7e21e4ca9687e978051980170688f
sty945 <sutaoyu@yeah.net>
2018-02-10 13:27:40 +0800

    将用户登录的方法由基于函数改为基于类方法

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
-from users.views import user_login
+from users.views import LoginView
 
 import xadmin
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url('^$', TemplateView.as_view(template_name="index.html"), name='index'),
-    url('^login/$', user_login, name='login')
+    url('^login/$', LoginView.as_view(), name='login')
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic import View
 # Create your views here.
 
 from .models import UserProfile
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html", {})
+
+    def post(self, request):
+        user_name = request.POST.get('username', '')
+        pass_word = request.POST.get('password', '')
+        user = authenticate(username=user_name, password=pass_word)
+        if user is not None:
+            login(request, user)
+            return render(request, "index.html")
+        else:
+            return render(request, "login.html", {"msg": "用户名或密码错误"})
 
 
 class CustomBackend(ModelBackend):
@@ -16,15 +32,3 @@ class CustomBackend(ModelBackend):
         except Exception as e:
             return None
 
-def user_login(request):
-    if request.method == "POST":
-        user_name = request.POST.get('username', '')
-        pass_word = request.POST.get('password', '')
-        user = authenticate(username=user_name, password=pass_word)
-        if user is not None:
-            login(request, user)
-            return render(request, "index.html")
-        else:
-            return render(request, "login.html", {"msg": "用户名或密码错误"})
-    elif request.method == 'GET':
-        return render(request, "login.html")
